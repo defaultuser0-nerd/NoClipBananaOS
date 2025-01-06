a3ac539a6f11670f6a90dcf4d0e0de15cbf34837
defaultuser0 <axolotlslmao@gmail.com>
2025-01-06 17:07:34 -0600

    fix

---
+++ NoClipBananaOS/Plugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BepInEx;
+using NoClipBananaOS;
+using UnityEngine;
+
+namespace NoClipBananaOS
+{
+    [BepInPlugin("com.Estatic.NoClip", "NoClip", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+	{
+    }
+}
